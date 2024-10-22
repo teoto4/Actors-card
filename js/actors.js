efe00a992dd1f4bb72f85cc0a608ff45505292f4
teoto4 <teoto@ukr.net>
2024-10-22 15:57:16 +0300

    first commit

---
+++ js/actors.js
@@ -0,0 +1,18 @@
+const actors = [
+    {   id:1,
+        name: "Andrew Neil",
+        photo:"./img/img4.jpg",
+        inst:"instagram.com",
+        inst_icon: "./img/new_insta.png"
+    },
+    {   id:2,
+        photo:"./img/img2.jpg",
+        name: "Jasmine Carter",
+        inst:"instagram.com"
+    },
+    {   id:3,
+        photo:"./img/img3.jpg",
+        name: "Justin Chung",
+        inst:"instagram.com"
+    }
+]
---
+++ js/index.js
@@ -0,0 +1,33 @@
+const actor_cards = document.querySelector('#actor_cards');
+console.log(actor_cards);
+
+
+function createCard(){
+    for(i = 0; i < actors.length; i++){
+        //create_element
+        const card = document.createElement('li');
+        const img = document.createElement('img');
+        const name = document.createElement('p');
+        const social = document.createElement('ul');
+        const social_a = document.createElement('a');
+        const  social_a_img = document.createElement('img')
+        //get_content
+        img.setAttribute("src", actors[i].photo);
+        name.textContent = actors[i].name;
+
+        social.classList.add('social_icon')
+        social_a.setAttribute("href", actors[i].inst); 
+        social_a_img.setAttribute('src', actor_cards.inst_icon)
+        //append
+        actor_cards.append(card);
+        card.append(img, name, social);
+        social.append(social_a);
+        social_a.append(social_a_img);
+        
+    }
+ 
+ console.dir(actor_cards);
+ 
+}
+
+createCard()
